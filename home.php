94fddc407dd4f5eabff36c0bc8128671f7093a2b
Patrick Pelayo <PatrickP@PatrickP.Tech>
2021-03-27 21:36:35 -0700

    the first content block is now responsive

--- home.php
+++ home.php
@@ -13,27 +13,27 @@
     </header>
 
     <main>
-        <div class='contentBox'>
-            <section class='container'>
-                <h1>Our Services</h1>
-                <div id='servicesRow' class='row'>
-                    <div class='col-lg-4'>
-                        <h2>Construction</h2>
-                        <p>We can build anything that you think of. We built buildings that reached the moon, and rocket ships that reached Europe.</p>
-                    </div>
-                    
-                    <div class='col-lg-4'>
-                        <h2>Maintenance</h2>
-                        <p>Our abilities to maintain things is unmatched. We maintain time machines so that we can always have a building like new.</p>
-                    </div>
-                    
-                    <div class='col-lg-4'>
-                        <h2>Demolition</h2>
-                        <p>Doesn't matter what it is, we can destroy it. We personally destroyed the Dinosaurs so that we can build more buildings.</p>
-                    </div>
+        
+        <section class='serviceBox container'>
+            <h1>Our Services</h1>
+            <div id='servicesRow' class='row'>
+                <div class='service col-lg-4'>
+                    <h2>Construction</h2>
+                    <p>We can build anything that you think of. We built buildings that reached the moon, and rocket ships that reached Europe.</p>
                 </div>
-            </section>
-        </div>
+
+                <div class='service col-lg-4'>
+                    <h2>Maintenance</h2>
+                    <p>Our abilities to maintain things is unmatched. We maintain time machines so that we can always have a building like new.</p>
+                </div>
+
+                <div class='service col-lg-4'>
+                    <h2>Demolition</h2>
+                    <p>Doesn't matter what it is, we can destroy it. We personally destroyed the Dinosaurs so that we can build more buildings.</p>
+                </div>
+            </div>
+        </section>
+        
         
 
 
